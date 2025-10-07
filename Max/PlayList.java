aacd8e6185bfc2c8d786222c30b0510b9e75bdff
meowwmurr <mkirill2006.km@gmail.com>
2025-10-07 21:49:31 +0300

    +queue

---
+++ Max/PlayList.java
@@ -0,0 +1,12 @@
+package Max;
+
+public class PlayList{
+    private class PlayListItem<Song>{
+        public Song song;
+        public PlayListItem<Song> next;
+        public PlayListItem(Song song, PlayListItem<Song> next){
+            this.song = song;
+            this.next = next;
+        }
+    }
+}
